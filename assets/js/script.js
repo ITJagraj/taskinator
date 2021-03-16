5d7a1aeebc317aae62149ab7a5c56c17c669224b
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-16 12:26:24 -0400

    initial taskinator setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-  });
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
